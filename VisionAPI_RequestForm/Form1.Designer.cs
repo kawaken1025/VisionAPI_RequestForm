0c5f695d525a7ef2a0ac628b7f4600f10b2a5d7c
kawaken1025 <aibasisyou090@gmail.com>
2020-08-23 13:58:27 +0900

    commit

---
+++ VisionAPI_RequestForm/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace VisionAPI_RequestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ReqButton = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "画像参照:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(503, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(521, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "参照";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(445, 367);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ReqButton
+            // 
+            this.ReqButton.Location = new System.Drawing.Point(582, 443);
+            this.ReqButton.Name = "ReqButton";
+            this.ReqButton.Size = new System.Drawing.Size(138, 33);
+            this.ReqButton.TabIndex = 4;
+            this.ReqButton.Text = "リクエスト";
+            this.ReqButton.UseVisualStyleBackColor = true;
+            this.ReqButton.Click += new System.EventHandler(this.ReqButton_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(465, 70);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(255, 367);
+            this.resultBox.TabIndex = 6;
+            this.resultBox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(463, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "OCR結果:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 486);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.ReqButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "GoogleVisionAPI リクエスト画面";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ReqButton;
+        private System.Windows.Forms.RichTextBox resultBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ VisionAPI_RequestForm/Form1.cs
@@ -0,0 +1,96 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using Google.Cloud.Vision.V1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisionAPI_RequestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 画像選択
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofDialog = new OpenFileDialog();
+
+            // デフォルトのフォルダを指定する
+            ofDialog.InitialDirectory = @"C:";
+            
+            //ダイアログのタイトルを指定する
+            ofDialog.Title = "画像選択";
+
+            //ダイアログを表示する
+            if (ofDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofDialog.FileName;
+                pictureBox1.ImageLocation = textBox1.Text;
+            }
+
+
+            // オブジェクトを破棄する
+            ofDialog.Dispose();
+        }
+
+        /// <summary>
+        /// Auth認証
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReqButton_Click(object sender, EventArgs e)
+        {
+            var projectId = "massive-runway-229116";
+            // If you don't specify credentials when constructing the client, the
+            // client library will look for credentials in the environment.
+            var credential = GoogleCredential.GetApplicationDefault();
+            var storage = StorageClient.Create(credential);
+            // Make an authenticated API request.
+            var buckets = storage.ListBuckets(projectId);
+            foreach (var bucket in buckets)
+            {
+                Console.WriteLine(bucket.Name);
+            }
+            RequestVisionAPI();
+        }
+
+        /// <summary>
+        /// VISION API へリクエストを送信
+        /// </summary>
+        private void RequestVisionAPI()
+        {
+            // 画像からリクエスト情報を生成
+            if (textBox1.Text.Length == 0) return;
+            var client = ImageAnnotatorClient.Create();
+            var image = Google.Cloud.Vision.V1.Image.FromFile(textBox1.Text);
+            var response = client.DetectText(image);
+
+            var resultText = "";
+            foreach (EntityAnnotation text in response)
+            {
+                //認識結果、言語、認識結果の各頂点を標準出力に表示
+                var coordinates = string.Join(",", text.BoundingPoly.Vertices.Select(v => $"({v.X},{v.Y})"));
+                Console.WriteLine($"Description: {text.Description} Locale:{text.Locale} coordinates:{coordinates}");
+                resultText += text.Description;
+            }
+            Console.WriteLine();
+            // 画面へ結果を表示
+            resultBox.Text = resultText;
+
+        }
+    }
+}
